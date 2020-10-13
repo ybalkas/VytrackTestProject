72ad3afa991e421b0d78c7bc2a33ec92dc9bee88
Yusuf Balkas <ybalkas@gmail.com>
2020-10-13 17:26:29 +0100

    ch1

--- src/test/java/Pages/myfirstclass.java
+++ src/test/java/Pages/myfirstclass.java
@@ -2,6 +2,8 @@ package Pages;
 
 public class myfirstclass {
     public static void main(String[] args) {
-        
+        System.out.println("x");
+        System.out.println("xy");
+        System.out.println("abc");
     }
 }
